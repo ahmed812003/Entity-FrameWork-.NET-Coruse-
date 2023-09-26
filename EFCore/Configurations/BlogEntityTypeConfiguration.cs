a2ef17f2ba5435f86ca6757c98b7eae368ad43fc
ahmed yaser <ay2309133@gmail.com>
2023-09-26 16:07:57 +0300

    Add files via upload

---
+++ EFCore/Configurations/BlogEntityTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Configurations
+{
+    public class BlogEntityTypeConfiguration : IEntityTypeConfiguration<Blog>
+    {
+        public void Configure(EntityTypeBuilder<Blog> builder) =>
+            builder.Property(p => p.Url).IsRequired();
+
+        public void DefultValueOfRating(EntityTypeBuilder<Blog> builder) =>
+            builder.Property(p => p.Rating).HasDefaultValue(2);
+
+        public void DefultValueOfAddedOn(EntityTypeBuilder<Blog> builder) =>
+            builder.Property(p => p.AddedOn).HasDefaultValueSql("GETDATE()");
+
+    }
+}
