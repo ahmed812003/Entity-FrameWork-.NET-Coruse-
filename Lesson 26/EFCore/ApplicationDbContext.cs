de93daabc0394f1edb15535cfc8b3682d34230f4
ahmed yaser <ay2309133@gmail.com>
2023-10-04 14:38:38 +0300

    Add files via upload

---
+++ Lesson 26/EFCore/ApplicationDbContext.cs
@@ -0,0 +1,54 @@
+﻿//using EFCore.Configurations;
+//using EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder) =>
+            optionBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectModels;Initial Catalog=EfCore;Integrated Security=True");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RecoredOfSale>().HasOne(r => r.Car).WithMany(c => c.SaleHistory).HasForeignKey(r => r.CarLicensePlate)
+                .HasPrincipalKey(c => c.LicensePlate);
+        }
+
+        public DbSet<Car> Cars { get; set; }
+
+    }
+}
+
+public class Car
+{
+    public int  CarId { get; set;}
+
+    public string LicensePlate { get; set; }
+
+    public string Make { get; set; }
+
+    public string Model { get; set; }
+
+    public List<RecoredOfSale> SaleHistory{ get; set; }
+
+}
+
+public class RecoredOfSale
+{
+    public int RecoredOfSaleId { get; set; }
+
+    public DateTime DateSold { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string CarLicensePlate { get; set;}
+
+    public Car Car { get; set; }
+}
---
+++ Lesson 26/EFCore/Migrations/20231004113352_First.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class First : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    CarId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LicensePlate = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Make = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.CarId);
+                    table.UniqueConstraint("AK_Cars_LicensePlate", x => x.LicensePlate);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecoredOfSale",
+                columns: table => new
+                {
+                    RecoredOfSaleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateSold = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CarLicensePlate = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecoredOfSale", x => x.RecoredOfSaleId);
+                    table.ForeignKey(
+                        name: "FK_RecoredOfSale_Cars_CarLicensePlate",
+                        column: x => x.CarLicensePlate,
+                        principalTable: "Cars",
+                        principalColumn: "LicensePlate",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecoredOfSale_CarLicensePlate",
+                table: "RecoredOfSale",
+                column: "CarLicensePlate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RecoredOfSale");
+
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
